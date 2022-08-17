21db0530401e93d356c56e6051cd914173804ec1
Zurceel <gabrielcruzsil@gmail.com>
2022-08-17 12:56:45 -0300

    Adicionar arquivos de projeto.

---
+++ HerancaAula/Program.cs
@@ -0,0 +1,35 @@
+﻿using Heranca;
+
+string nome;
+string especie;
+
+Console.WriteLine("Informe o nome do mamífero:");
+nome = Console.ReadLine();
+
+Console.WriteLine("Informe a especie:");
+especie = Console.ReadLine();
+
+Mamifero mamifero = new Mamifero(nome, especie);
+
+mamifero.Mamar();
+
+Console.WriteLine("Informe o nome do réptil:");
+nome = Console.ReadLine();
+
+Console.WriteLine("Informe a especie:");
+especie = Console.ReadLine();
+
+Console.WriteLine("Informe se o répitil possui veneno: (Sim/Nao) ");
+string respostaveneno = Console.ReadLine();
+
+Reptil reptil = new Reptil(nome, especie);
+if (respostaveneno == "Sim")
+{
+	reptil.PossuiVeneno = true;
+}
+else
+{
+	reptil.PossuiVeneno = false;
+}
+
+reptil.BotarOvo();
---
+++ HerancaAula/Reptil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca
+{
+	public class Reptil : Animal
+	{
+		public bool PossuiVeneno { get; set; }
+
+		public Reptil(string nome, string especie)
+		{
+			Nome = nome;
+			Especie = especie;
+		}
+
+		public void BotarOvo()
+		{
+			if (PossuiVeneno)
+			{
+				Console.WriteLine($"Aninal {Nome} da espécie {Especie} está botando ovo e é venenoso!");
+			}
+			else
+			{
+				Console.WriteLine($"Aninal {Nome} da espécie {Especie} está botando ovo e não é venenoso!");
+			}
+		}
+	}
+}
